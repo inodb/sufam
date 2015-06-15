ce6b032f838d129f83d523dbfd4a51e67dd03068
Ino de Bruijn <ino@ino.pm>
2015-06-15 17:32:29 -0400

    Add versioneer for tag based versioning
    Versioneer (https://github.com/warner/python-versioneer) uses the
    tag from the VCS to set the version of the python package.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup
 import sys, os
-
-version = '0.0.1'
+import versioneer
 
 setup(name='sufam',
-      version=version,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description="So U Found A Mutation?",
-      long_description="""Validate mutations from supplied vcf in supplied bam""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='Python validation mutation mpileup samtools pileup vcf bam',
       author='Ino de Bruijn',
@@ -15,10 +14,8 @@ setup(name='sufam',
       maintainer='Ino de Bruijn',
       maintainer_email='ino@ino.pm',
       url='https://github.com/inodb/sufam',
-      license='FreeBSD',
+      download_url = 'https://github.com/inodb/sufam/tarball/'+versioneer.get_version(),
       packages=['sufam'],
-      include_package_data=True,
-      zip_safe=False,
       install_requires=['numpy>=1.9.2',
                         'pandas>=0.16.1',
                         ],
