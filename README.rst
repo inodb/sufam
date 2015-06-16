41db9acb14380851ed795b73fbafb8f9d1cee609
Ino de Bruijn <ino@ino.pm>
2015-06-16 11:04:25 -0400

    Change packaging based on @hypnek's blog
    Change packaging following the blog post by hypnek:
    
    https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/

---
+++ README.rst
@@ -0,0 +1,34 @@
+So U Found A Mutation? (SUFAM)
+==============================
+
+Found a mutation in one or more samples? Now you want to check if they are in
+another sample. Unfortunately mutect, varscan or whatever other variant caller
+is not calling them. Use SUFAM. The super sensitive validation caller that
+calls everything on a given position. All you need is a vcf with the mutations
+that you are interested in and the sam/bam file of the sample where you want to
+find the same inconsipicuous mutation.
+
+Installation
+------------
+::
+
+    pip install sufam
+
+Run
+---
+::
+
+    usage: sufam [-h] [--sample_name SAMPLE_NAME] [--format {matrix,sufam}]
+                reffa vcf bam
+
+    positional arguments:
+    reffa                 Reference genome (fasta)
+    vcf                   VCF with mutations to be validated
+    bam                   BAM to find mutations in
+
+    optional arguments:
+    -h, --help            show this help message and exit
+    --sample_name SAMPLE_NAME
+                            Set name of sample, used in output.
+    --format {matrix,sufam}
+                            Set output format
--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys, os
 import versioneer
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
 setup(name='sufam',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       description="So U Found A Mutation?",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=read("README.rst"),
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
       keywords='Python validation mutation mpileup samtools pileup vcf bam',
       author='Ino de Bruijn',
       author_email='ino@ino.pm',
-      maintainer='Ino de Bruijn',
-      maintainer_email='ino@ino.pm',
       url='https://github.com/inodb/sufam',
-      download_url = 'https://github.com/inodb/sufam/tarball/'+versioneer.get_version(),
-      packages=['sufam'],
+      license="MIT",
+      include_package_data=True,
+      packages=find_packages(exclude=['test*']),
       install_requires=['numpy>=1.9.2',
                         'pandas>=0.16.1',
                         ],
