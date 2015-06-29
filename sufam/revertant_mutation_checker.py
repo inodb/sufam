d0aab519d7a9b686eef004fc13786fe35ab5a502
Ino de Bruijn <ino@ino.pm>
2015-06-29 16:13:40 -0400

    Change hgvs coords sequentially, add germline muts
    Change coords of putative revertant hgvs mutation if required by del/ins of the
    mutation that is to be reverted. Add extra to be reverted mutations (in this
    case germline mutations).

--- sufam/revertant_mutation_checker.py
+++ sufam/revertant_mutation_checker.py
@@ -10,6 +10,54 @@ import pyhgvs as hgvs
 from Bio import SeqIO
 
 
+def alter_coords_hgvs_sequential(h1, h2):
+    """Change HGVS coords of h2 after applying h1"""
+    if h1.kind == "c" and h2.kind == "c":
+        if h1.mutation_type == ">":
+            h3 = h2
+        elif h1.mutation_type == "del":
+            if h1.cdna_start.coord > h2.cdna_end.coord:
+                h3 = h2
+            elif h1.cdna_end.coord < h2.cdna_start.coord:
+                h3 = h2
+                h3.cdna_start = hgvs.CDNACoord(coord=h3.cdna_start.coord-len(h1.ref_allele))
+                h3.cdna_end = hgvs.CDNACoord(coord=h3.cdna_end.coord-len(h1.ref_allele))
+            else:
+                raise(Exception("Overlapping del not implemented"))
+        elif h1.mutation_type == "ins":
+            if h1.cdna_start.coord > h2.cdna_end.coord:
+                h3 = h2
+            elif h1.cdna_end.coord < h2.cdna_start.coord:
+                h3 = h2
+                h3.cdna_start = hgvs.CDNACoord(coord=h3.cdna_start.coord+len(h1.alt_allele))
+                h3.cdna_end = hgvs.CDNACoord(coord=h3.cdna_end.coord+len(h1.alt_allele))
+            else:
+                raise(Exception("Overlapping ins not implemented"))
+        elif h1.mutation_type == "dup":
+            if h1.cdna_start.coord > h2.cdna_end.coord:
+                h3 = h2
+            elif h1.cdna_end.coord < h2.cdna_start.coord:
+                h3 = h2
+                h3.cdna_start = hgvs.CDNACoord(coord=h3.cdna_start.coord+len(h1.alt_allele))
+                h3.cdna_end = hgvs.CDNACoord(coord=h3.cdna_end.coord+len(h1.alt_allele))
+            else:
+                raise(Exception("Overlapping dup not implemented"))
+        elif h1.mutation_type == "delins":
+            if h1.cdna_start.coord > h2.cdna_end.coord:
+                h3 = h2
+            elif h1.cdna_end.coord < h2.cdna_start.coord:
+                h3 = h2
+                h3.cdna_start = hgvs.CDNACoord(coord=h3.cdna_start.coord-len(h1.ref_allele)+len(h1.alt_allele))
+                h3.cdna_end = hgvs.CDNACoord(coord=h3.cdna_end.coord-len(h1.ref_allele)+len(h1.alt_allele))
+            else:
+                raise(Exception("Overlapping delins not implemented"))
+        else:
+            raise(Exception("Unexpected mutation_type {}".format(h1.mutation_type)))
+        return h3
+    else:
+        raise(Exception("Only cDNA mutations have been implemented"))
+
+
 def apply_hgvs(seq, h):
     """Apply 1-based HGVS mutation to 0-based biopython Sequence"""
     if h.kind == "c":
@@ -17,20 +65,24 @@ def apply_hgvs(seq, h):
         end = h.cdna_end.coord - 1
         if h.mutation_type == ">":
             assert(seq[start] == h.ref_allele)
-            seq = seq[start] = h.alt_allele
+            new_seq = seq[:start] + h.alt_allele + seq[start+1:]
         elif h.mutation_type == "del":
             assert(seq[start:end+1] == h.ref_allele)
-            seq = seq[:start] + seq[end:]
+            new_seq = seq[:start] + seq[end+1:]
+            assert(len(seq) == len(new_seq) + len(h.ref_allele))
         elif h.mutation_type == "ins":
-            assert(seq[start] == h.ref_allele)
-            # TODO: is this correct?
-            seq = seq[:start] + h.alt_allele + seq[end:]
+            new_seq = seq[:start+1] + h.alt_allele + seq[end:]
+            assert(len(seq) + len(h.alt_allele) == len(new_seq))
         elif h.mutation_type == "dup":
             assert(seq[start:end+1] == h.ref_allele)
-            seq = seq[:start] + h.alt_allele + seq[end:]
+            new_seq = seq[:end+1] + h.alt_allele + seq[end+1:]
+        elif h.mutation_type == "delins":
+            assert(seq[start:end+1] == h.ref_allele)
+            new_seq = seq[:start] + h.alt_allele + seq[end+1:]
+            assert(len(seq) - len(h.ref_allele) + len(h.alt_allele) == len(new_seq))
         else:
             raise(Exception("Unexpected mutation_type {}".format(h.mutation_type)))
-        return seq
+        return new_seq
     else:
         raise(Exception("Only cDNA mutations have been implemented"))
 
@@ -52,14 +104,12 @@ def is_revertant(record, hgvs_mut, hgvs_rev_mut):
 
 def check_revertant_mutations(vcf, oncotator_file, fasta):
     ot = oncotator.Oncotator(oncotator_file)
-    transcripts = \
-        SeqIO.to_dict(SeqIO.parse("tests/test_data/BRCA1_transcripts.fa",
-        "fasta"))
+    transcripts = SeqIO.to_dict(SeqIO.parse(fasta, "fasta"))
 
     for m in [l.rstrip('\n') for l in open(vcf).readlines()]:
         hgvs_mut = hgvs.HGVSName(m)
         for hgvs_rev_mut in ot.get_hgvs_mutations(hgvs_mut.transcript):
-            if is_revertant(transcripts[hgvs_mut.transcript], hgvs_mut, hgvs_rev_mut):
+            if is_revertant(transcripts[hgvs_mut.transcript], hgvs_mut, alter_coords_hgvs_sequential(hgvs_mut, hgvs_rev_mut)):
                 logging.info("REVERTANT MUTATION FOUND ZOMG")
 
 
@@ -72,7 +122,7 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("vcf", type=str, help="VCF with mutation to be reverted")
-    parser.add_argument("oncotator_file", type=str, help="MAF from Oncotator to find revertant mutations in")
+    parser.add_argument("oncotator_file", type=str, help="MAF to find mutations in")
     parser.add_argument("fasta", type=str, help="Fasta file with transcripts")
     args = parser.parse_args()
     check_revertant_mutations(args.vcf, args.oncotator_file, args.fasta)
